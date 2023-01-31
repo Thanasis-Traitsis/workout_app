95712b32abc35a0e1ff75618bf61e453cc91874f
Thanasis Traitsis <73651594+Thanasis-Traitsis@users.noreply.github.com>
2023-01-31 16:15:53 +0200

    Create basic models exercise & workout

---
+++ lib/models/workout.dart
@@ -0,0 +1,30 @@
+import 'exercise.dart';
+
+class Workout {
+  final String? title;
+  final List<Exercise> exercises;
+
+  const Workout({
+    required this.title,
+    required this.exercises,
+  });
+
+  factory Workout.fromJson(Map<String, dynamic> json) {
+    List<Exercise> exercises = [];
+    int index = 0;
+    int startTime = 0;
+
+    for (var ex in json['exercises'] as Iterable) {
+      exercises.add(Exercise.fromJson(json, index, startTime));
+      index++;
+      startTime += exercises.last.prelude! + exercises.last.duration!;
+    }
+
+    return Workout(title: json['title'] as String?, exercises: exercises);
+  }
+
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'exercises': exercises,
+      };
+}
